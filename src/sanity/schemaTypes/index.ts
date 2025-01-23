84b211ead762c4e7e878695dd2d33489822c5b1b
Tanzeel Sarwar <tanzeelsarwar486@gmail.com>
2025-01-23 23:39:50 +0500

    Add files via upload

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,7 +1,10 @@
-import { type SchemaTypeDefinition } from "sanity";
-import { productSchema } from "./products";
-import { categorySchema } from "./categories";
+import type { SchemaTypeDefinition } from "sanity"
+import { productSchema } from "./products"
+import { categorySchema } from "./categories"
+import { orderSchema } from "./orders"
+import { adminUserSchema } from "./adminUser"
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [productSchema, categorySchema],
-};
+  types: [productSchema, categorySchema, orderSchema, adminUserSchema],
+}
+
